c35bc22568f72f1f74a3a807d1b08cbb608cc6bd
biglulu3310 <36730433+biglulu3310@users.noreply.github.com>
2024-10-21 20:06:29 +0800

    version2

--- src/fade-in.js
+++ src/fade-in.js
@@ -4,14 +4,28 @@ $(document).ready(function() {
     $(window).scroll( function(){
     
         /* Check the location of each desired element */
-        $('.hideme').each( function(i){
+        $('.hideme1').each( function(i){
             
-            var bottom_of_object = 1.02*($(this).offset().top + $(this).outerHeight());
+            var bottom_of_object = 1*($(this).offset().top + $(this).outerHeight());
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             
             /* If the object is completely visible in the window, fade it it */
-            if( bottom_of_window > bottom_of_object ){                
-                $(this).animate({'opacity':'1', 'width': '100%'},1200);
+            if( bottom_of_window > bottom_of_object ){ 		
+					$(this).delay(20).animate({'opacity':'1', 'margin-left':'0'},850);
+				
+            }
+            
+        }); 
+		
+		$('.hideme2').each( function(i){
+            
+            var bottom_of_object = 1*($(this).offset().top + $(this).outerHeight());
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){ 		
+					$(this).delay(20).animate({'opacity':'1', 'margin-left':'0'},850);
+				
             }
             
         }); 
